c69f87d31fe074fe579cc22bbabf33528f5dd29a
Mateusz Dziubek <mateusz.dziubek@schibsted.pl>
2017-10-13 14:48:11 +0200

    Now user can take and save picture to app's internal folder

--- app/src/main/java/com/beerai/MainActivity.kt
+++ app/src/main/java/com/beerai/MainActivity.kt
@@ -1,19 +1,50 @@
 package com.beerai
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.support.v4.content.FileProvider
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.toolbar.*
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    val REQUEST_IMAGE_CAPTURE = 1
+    lateinit var currentPhotoPath: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        scan_label_button.setOnClickListener {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(packageManager) != null) {
+                var photoFile: File? = null
+                try {
+                    photoFile = createImageFile()
+                } catch (e: IOException) {
+                    Toast.makeText(this, getString(R.string.file_error_text), Toast.LENGTH_SHORT).show()
+                }
+                if (photoFile != null) {
+                    val photoURI = FileProvider.getUriForFile(this,
+                            "com.beerai.fileprovider", photoFile)
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -29,5 +60,25 @@ class MainActivity : AppCompatActivity() {
             }
             else -> return super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        val imageFileName = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.ENGLISH).format(Date())
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        )
+        currentPhotoPath = image.absolutePath
+        return image
     }
 }
