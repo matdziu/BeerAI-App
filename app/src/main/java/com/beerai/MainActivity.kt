5326c716acddf840338e0e4eb135844af14377fb
Mateusz Dziubek <mateusz.dziubek@schibsted.pl>
2017-10-13 10:50:20 +0200

    Added expandable search view

--- app/src/main/java/com/beerai/MainActivity.kt
+++ app/src/main/java/com/beerai/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.beerai
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.widget.SearchView
+import kotlinx.android.synthetic.main.toolbar.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu?.findItem(R.id.search_item)?.actionView as SearchView
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.setIconifiedByDefault(false)
+
+        return true
     }
 }
